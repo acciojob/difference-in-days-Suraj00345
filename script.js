9188c00ee654209741ef4c3896da65db641c76b4
Suraj Kumar Manna <126694317+Suraj00345@users.noreply.github.com>
2023-11-15 09:27:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here
+   var Difference_In_Time = date2.getTime() - date1.getTime();
+	 var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+	console.log(Difference_In_Days);
 };
 
 // Do not change the code below.
