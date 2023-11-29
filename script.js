f062ee528a0c9771be9884e888a98070f7780011
Suraj Kumar Manna <126694317+Suraj00345@users.noreply.github.com>
2023-11-29 18:47:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var dateDiffInDays = function (date1, date2) {
    var Difference_In_Time = date2.getTime() - date1.getTime();
-	 var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
-	console.log(Difference_In_Days);
+   var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+   return Difference_In_Days;
 };
 
 // Do not change the code below.
-const dateOne = prompt("Enter Start Date.");
-const dateTwo = prompt("Enter End Date.");
+const dateOne = new Date(prompt("Enter Start Date."));
+const dateTwo = new Date(prompt("Enter End Date."));
 alert(dateDiffInDays(dateOne, dateTwo));
+
